4c139d38d8575dc354a48d28453f63358c9a1c9e
shifi <shifi@seminar.com>
2025-01-21 11:23:43 +0200

    read and write with tests

--- services/DB/read.js
+++ services/DB/read.js
@@ -1,5 +1,5 @@
-const fs=require('fs')
-const path=require('path')
+const fs = require('fs')
+const path = require('path')
 require('dotenv').config()
 
 // הספריה שמטפלת ב env
@@ -8,23 +8,25 @@ require('dotenv').config()
 //שומר סיסמאות/נתונים מוצפנים שלא עולים ב גיט אלא נמצאים בדיסק המקומי
 //config מוציא את הנתונים מהenv
 
-const {DATA_BASE_PATH='C:/data-base-temp'}=process.env
+const { DATA_BASE_PATH = 'C:/data-base-temp' } = process.env
 
 function getAll(model) {
-    
+
     try {
-        const filePath=path.join(DATA_BASE_PATH,`${model}.json`)
-        if(!fs.existsSync(filePath))
+        if(!model instanceof String){
+            throw Error('the model must be string')
+        }
+        const filePath = path.join(DATA_BASE_PATH, `${model}.json`)
+        if (!fs.existsSync(filePath))
             return []
-        const data=fs.readFileSync(filePath)
-        const list=JSON.parse(list)
-        if(Array.isArray(list)){
+        const data = fs.readFileSync(filePath)
+        const list = JSON.parse(data)
+        if (Array.isArray(list)) {
             return list
         }
-        else{
-            throw Error (`the data in ${filePath} is corrupt`)
+        else {
+            throw Error(`the data in ${filePath} is corrupt`)
         }
-        
     }
     catch (err) {
         throw err
@@ -32,6 +34,21 @@ function getAll(model) {
 }
 
 function getByCondition(model, condition) {
+    try {
+        if(!condition instanceof Object){
+            throw Error ('the condition must be Object')
+        }
+        const collection = getAll(model)
+        if (condition) {
+            const response = collection.filter(item => Object.entries(condition).every(([key, value]) => item[key] && item[key] === value))
+            return response
+        }
+        else
+            return collection
+    }
+    catch (error) {
+        throw error
+    }
 
 }
 
---
+++ test/services/DB/write.test.js
@@ -0,0 +1,84 @@
+const { addOne } = require('../../../services/DB/write')
+
+const fs = require("fs")
+const path = require("path")
+const { getAll } = require('../../../services/DB/read')
+
+
+jest.mock('fs')
+jest.mock('path')
+jest.mock('../../../services/DB/read')
+
+describe('ADD ONE', () => {
+    afterEach(() => {
+        getAll.mockReset()
+        path.join.mockReset()
+        fs.existsSync.mockReset()
+        fs.writeFileSync.mockReset()
+        fs.mkdirSync.mockReset()
+    })
+    it('should add a new item to an existing data base file', () => {
+        const model = 'test-point'
+        getAll.mockReturnValue([{ x: 5, y: 7 }])
+        fs.existsSync.mockReturnValue(true)
+        path.join.mockReturnValue(`/folder/${model}.json`)
+
+        _ = addOne(model, { x: 9, y: 2 })
+
+        expect(getAll).toHaveBeenCalledWith(model)
+        expect(fs.existsSync).toHaveBeenCalledWith('C:/data-base-temp')
+        expect(fs.mkdirSync).not.toHaveBeenCalled()
+        expect(path.join).toHaveBeenCalledWith('C:/data-base-temp', `${model}.json`)
+        expect(fs.writeFileSync).toHaveBeenCalledWith(`/folder/${model}.json`, JSON.stringify([{ x: 5, y: 7 }, { x: 9, y: 2 }]))
+    })
+
+    it('should insert the first data to the dataBase ', () => {
+        const model = 'test-point'
+        getAll.mockReturnValue([{ x: 5, y: 7 }])
+        fs.existsSync.mockReturnValue(false)
+        fs.mkdirSync.mockReturnValue('data-base-temp')
+        path.join.mockReturnValue(`/folder/${model}.json`)
+
+        _ = addOne(model, { x: 9, y: 2 })
+
+        expect(getAll).toHaveBeenCalledWith(model)
+        expect(fs.existsSync).toHaveBeenCalledWith('C:/data-base-temp')
+        expect(fs.mkdirSync).toHaveBeenCalledWith('C:/data-base-temp', { recursive: true })
+        expect(path.join).toHaveBeenCalledWith('C:/data-base-temp', `${model}.json`)
+        expect(fs.writeFileSync).toHaveBeenCalledWith(`/folder/${model}.json`, JSON.stringify([{ x: 5, y: 7 }, { x: 9, y: 2 }]))
+    })
+    describe('ERRORS', () => {
+        it('should throw error when fs.writeFileSync throws error', () => {
+            const model = 'test-point'
+            getAll.mockReturnValue([{ x: 5, y: 7 }])
+            fs.existsSync.mockReturnValue(true)
+            path.join.mockReturnValue(`/folder/${model}.json`)
+            fs.writeFileSync.mockImplementation(() => { throw Error('error from mock') })
+            expect(() => addOne('test')).toThrow('error from mock')
+        })
+        it('should throw error when path.join throws error', () => {
+            getAll.mockReturnValue([{ x: 5, y: 7 }])
+            fs.existsSync.mockReturnValue(true)
+            path.join.mockImplementation(() => { throw Error('error from mock') })
+            expect(() => addOne('test')).toThrow('error from mock')
+        })
+        it('should throw error when fs.mkdirsync throws error', () => {
+            getAll.mockReturnValue([{ x: 5, y: 7 }])
+            fs.existsSync.mockReturnValue(false)
+            fs.mkdirSync.mockImplementation(() => { throw Error('error from mock') })
+            expect(() => addOne('test')).toThrow('error from mock')
+        })
+        it('should throw error when getAll throws error', () => {
+            getAll.mockImplementation(() => { throw Error('error from mock') })
+            expect(() => addOne('test')).toThrow('error from mock')
+        })
+        it('should throw error when item is undefined', () => {
+            // getAll.mockReturnValue([{ x: 5, y: 7 }])
+
+            // expect(()=>addOne('test')).toThrow('item must be defined')
+        })
+        it('should throw error when model is not a string', () => {
+              
+        })
+    })
+})
